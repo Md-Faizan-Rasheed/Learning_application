f5b2a2fcd4739ac8fa6b657052174369c363df68
Md-Faizan-Rasheed <s1391792@gmail.com>
2026-08-20 21:42:05 +0530

    change the cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="Islamic Learning Game API")
 if settings.env == "development":
     app.add_middleware(
         CORSMiddleware,
-        allow_origin_regex=["*"],
+        allow_origin_regex=".*",
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
